1448fd77664db391909c6e71003bddb643fe8eb0
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-11 20:00:10 +0530

    Time: 10 ms (50%), Space: 42.4 MB (80.51%) - LeetHub

--- 2406-decode-the-message/2406-decode-the-message.java
+++ 2406-decode-the-message/2406-decode-the-message.java
@@ -1,22 +1,42 @@
 class Solution {
     public String decodeMessage(String key, String message) {
-        HashMap<Character, Character>decoder = new HashMap<>();
-        char alpha = 'a';
-        StringBuilder output = new StringBuilder();
+
+        // Approach 1 Using Hashing
+        // HashMap<Character, Character>decoder = new HashMap<>();
+        // char alpha = 'a';
+        // StringBuilder output = new StringBuilder();
         
-        for(int i=0; i<key.length();i++) {
-            if(decoder.containsKey(key.charAt(i))==false && key.charAt(i) != ' ') {
-                decoder.put(key.charAt(i), alpha++);
-            }
-        }
+        // for(int i=0; i<key.length();i++) {
+        //     if(decoder.containsKey(key.charAt(i))==false && key.charAt(i) != ' ') {
+        //         decoder.put(key.charAt(i), alpha++);
+        //     }
+        // }
 
-        for(int i=0; i<message.length(); i++) {
-            if(decoder.containsKey(message.charAt(i)))
-                output.append(decoder.get(message.charAt(i)));
-            else
-                output.append(' ');
-        }
+        // for(int i=0; i<message.length(); i++) {
+        //     if(decoder.containsKey(message.charAt(i)))
+        //         output.append(decoder.get(message.charAt(i)));
+        //     else
+        //         output.append(' ');
+        // }
 
+        // return output.toString();
+
+        // Approach 2 Without Using Hashing
+        ArrayList<Character> decoder = new ArrayList<Character>();
+	    StringBuilder output = new StringBuilder();
+	    
+	    for(int i=0;i<key.length();i++) {
+	        if(key.charAt(i) != ' ' && decoder.indexOf(key.charAt(i)) == -1)
+	            decoder.add(key.charAt(i));
+	    }
+	    
+	    for (int i=0; i<message.length(); i++) {
+	        if(message.charAt(i) == ' ')
+	            output.append(' ');
+	        else
+	            output.append((char)(decoder.indexOf(message.charAt(i))+97));
+	    }
+	    
         return output.toString();
         
     }
