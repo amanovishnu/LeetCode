652e1efa59a89491e54f872a2844256c0ebf1f68
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-11 17:22:23 +0530

    Time: 50 ms (7.32%), Space: 16.3 MB (10.34%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,12 +1,20 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        jewel_box = {}
+
+        # Apporach 1 Using Dictionary
+        # jewel_box = {}
+        # output = 0
+        # for stone in stones:
+        #     if stone in jewel_box:
+        #         jewel_box[stone] += 1
+        #     else:
+        #         jewel_box[stone] = 1              
+        # for jewel in jewels:
+        #     output += jewel_box.get(jewel,0)         
+        # return output
+
+        # Apporach 2 Using Regular Expressions
         output = 0
-        for stone in stones:
-            if stone in jewel_box:
-                jewel_box[stone] += 1
-            else:
-                jewel_box[stone] = 1              
         for jewel in jewels:
-            output += jewel_box.get(jewel,0)         
+            output += len(re.findall(jewel, stones))
         return output
