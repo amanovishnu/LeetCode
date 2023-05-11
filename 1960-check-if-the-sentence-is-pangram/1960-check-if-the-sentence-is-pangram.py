15518185b6a932bcadee0c80a99425e7dd8decb4
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-11 22:08:53 +0530

    Time: 42 ms (21.05%), Space: 16.4 MB (12.74%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -1,12 +1,21 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        
-        alpha_dict = {}
-        for ele in range(0,26):
-            alpha_dict[chr(ele+97)] = 0
-            
-        for ele in sentence:
-            if ele in alpha_dict:
-                alpha_dict[ele] += 1
 
-        return False if 0 in alpha_dict.values() else True
+        # Apporach 1 Using Hashing
+        # alpha_dict = {}
+        # for ele in range(0,26):
+        #     alpha_dict[chr(ele+97)] = 0
+            
+        # for ele in sentence:
+        #     if ele in alpha_dict:
+        #         alpha_dict[ele] += 1
+
+        # return False if 0 in alpha_dict.values() else True
+
+        # Approach 2 Using Lists
+        alpha_list = [0 for i in range(26)]
+
+        for ele in sentence:
+            alpha_list[ord(ele)-97] = ele
+        
+        return False if 0 in alpha_list else True
