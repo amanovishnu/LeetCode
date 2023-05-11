5b29628fe3e10bd75aa47192c724d0ba06d613e9
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-11 22:12:27 +0530

    Time: 47 ms (12.97%), Space: 16.3 MB (12.74%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -18,4 +18,9 @@ class Solution:
         for ele in sentence:
             alpha_list[ord(ele)-97] = ele
         
-        return False if 0 in alpha_list else True
+        # return False if 0 in alpha_list else True
+
+        for ele in alpha_list:
+            if ele == 0:
+                return False
+        return True
