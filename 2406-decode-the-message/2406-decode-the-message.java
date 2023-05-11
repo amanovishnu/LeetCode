4bf4adefc120bf23bad04aefb5acd2cb8a0b1eb3
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-11 20:02:15 +0530

    Time: 8 ms (60.95%), Space: 42.8 MB (44.07%) - LeetHub

--- 2406-decode-the-message/2406-decode-the-message.java
+++ 2406-decode-the-message/2406-decode-the-message.java
@@ -2,42 +2,42 @@ class Solution {
     public String decodeMessage(String key, String message) {
 
         // Approach 1 Using Hashing
-        // HashMap<Character, Character>decoder = new HashMap<>();
-        // char alpha = 'a';
-        // StringBuilder output = new StringBuilder();
+        HashMap<Character, Character>decoder = new HashMap<>();
+        char alpha = 'a';
+        StringBuilder output = new StringBuilder();
         
-        // for(int i=0; i<key.length();i++) {
-        //     if(decoder.containsKey(key.charAt(i))==false && key.charAt(i) != ' ') {
-        //         decoder.put(key.charAt(i), alpha++);
-        //     }
-        // }
+        for(int i=0; i<key.length();i++) {
+            if(decoder.containsKey(key.charAt(i))==false && key.charAt(i) != ' ') {
+                decoder.put(key.charAt(i), alpha++);
+            }
+        }
 
-        // for(int i=0; i<message.length(); i++) {
-        //     if(decoder.containsKey(message.charAt(i)))
-        //         output.append(decoder.get(message.charAt(i)));
-        //     else
-        //         output.append(' ');
-        // }
+        for(int i=0; i<message.length(); i++) {
+            if(decoder.containsKey(message.charAt(i)))
+                output.append(decoder.get(message.charAt(i)));
+            else
+                output.append(' ');
+        }
 
-        // return output.toString();
+        return output.toString();
 
         // Approach 2 Without Using Hashing
-        ArrayList<Character> decoder = new ArrayList<Character>();
-	    StringBuilder output = new StringBuilder();
+        // ArrayList<Character> decoder = new ArrayList<Character>();
+	    // StringBuilder output = new StringBuilder();
 	    
-	    for(int i=0;i<key.length();i++) {
-	        if(key.charAt(i) != ' ' && decoder.indexOf(key.charAt(i)) == -1)
-	            decoder.add(key.charAt(i));
-	    }
+	    // for(int i=0;i<key.length();i++) {
+	    //     if(key.charAt(i) != ' ' && decoder.indexOf(key.charAt(i)) == -1)
+	    //         decoder.add(key.charAt(i));
+	    // }
 	    
-	    for (int i=0; i<message.length(); i++) {
-	        if(message.charAt(i) == ' ')
-	            output.append(' ');
-	        else
-	            output.append((char)(decoder.indexOf(message.charAt(i))+97));
-	    }
+	    // for (int i=0; i<message.length(); i++) {
+	    //     if(message.charAt(i) == ' ')
+	    //         output.append(' ');
+	    //     else
+	    //         output.append((char)(decoder.indexOf(message.charAt(i))+97));
+	    // }
 	    
-        return output.toString();
+        // return output.toString();
         
     }
 }
