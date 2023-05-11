b869da8ad76066ddbd2f8ecee1411d8589946461
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-11 19:44:47 +0530

    Time: 59 ms (5.89%), Space: 16.4 MB (8.17%) - LeetHub

--- 2406-decode-the-message/2406-decode-the-message.py
+++ 2406-decode-the-message/2406-decode-the-message.py
@@ -1,14 +1,33 @@
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        decoder = {}
-        alpha = 96
-        output = ""
-        for idx, ele in enumerate(key):
-            if ele not in decoder and ele != " ":
-                alpha += 1
-                decoder[ele] = chr(alpha)
+        
+        # Approach 1 Using Hashing
+        # decoder = {}
+        # alpha = 96
+        # output = ""
+        
+        # for idx, ele in enumerate(key):
+        #     if ele not in decoder and ele != " ":
+        #         alpha += 1
+        #         decoder[ele] = chr(alpha)
                 
-        for char in message:
-            output += decoder[char] if char in decoder else " "
+        # for char in message:
+        #     output += decoder[char] if char in decoder else " "
             
-        return (output)
+        # return (output)
+
+        # Apporach 2 Without using Hashing
+        index = []
+        output = ''
+
+        for ele in key:
+            if ele != ' ' and ele not in index:
+                index.append(ele)
+
+        for ele in message:
+            if ele != ' ':
+                output += chr(index.index(ele)+97)
+            else:
+                output += ' '
+
+        return output
