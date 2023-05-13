b24bd38387b935624c48b417ddde60df46085799
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-13 17:25:56 +0530

    Time: 54 ms (12.10%), Space: 16.4 MB (9.37%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -8,11 +8,21 @@ class Solution:
         
         #Optimized Apporach
         vowels = {"a","e","i","o","u","A","E","I","O","U"}
-        lCount, rCount = 0,0
-        left, right = 0, len(s)-1
-        while(left <right):
-            lCount += 1 if s[left] in vowels else 0
-            rCount += 1 if s[right] in vowels else 0
-            left+=1
-            right-=1
-        return lCount==rCount
+        # lCount, rCount = 0,0
+        # left, right = 0, len(s)-1
+        # while(left <right):
+        #     lCount += 1 if s[left] in vowels else 0
+        #     rCount += 1 if s[right] in vowels else 0
+        #     left+=1
+        #     right-=1
+        # return lCount==rCount
+        
+        a = s[:len(s)//2]
+        b = s[len(s)//2:]
+        a_count, b_count = 0, 0
+        for i in range(len(a)):
+            if a[i] in vowels:
+                a_count += 1
+            if b[i] in vowels:
+                b_count += 1
+        return a_count == b_count
