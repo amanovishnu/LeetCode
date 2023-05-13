b1e6492f9bb2874af8bae43170306757f40bb69b
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-13 15:26:17 +0530

    Time: 829 ms (85.63%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,6 +1,14 @@
 # Write your MySQL query statement below
+
+# SELECT name
+# FROM Customer
+# WHERE referee_id IS NULL
+# OR referee_id != 2
+# ORDER BY name ASC;
+
 SELECT name
 FROM Customer
-WHERE referee_id IS NULL
-OR referee_id != 2
+WHERE referee_id != 2
+OR referee_id IS NULL
 ORDER BY name ASC;
+
