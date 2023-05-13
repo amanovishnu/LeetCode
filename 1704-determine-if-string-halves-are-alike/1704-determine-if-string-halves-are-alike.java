1ff6917fcd1843afdb79c93929a7700b144f4bf7
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-13 18:20:47 +0530

    Time: 7 ms (32.05%), Space: 40.9 MB (72.51%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,14 +1,22 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-		Set VOWELS = Set.of('a','e','i','o','u','A','E','I','O','U');
-		int left = 0, right = s.length()-1;
+		// Set VOWELS = Set.of('a','e','i','o','u','A','E','I','O','U');
+		// int left = 0, right = s.length()-1;
+		// int lCount = 0, rCount = 0;
+		// while(left<right){
+		//     lCount += VOWELS.contains(s.charAt(left)) ? 1 : 0;
+		//     rCount += VOWELS.contains(s.charAt(right)) ? 1 : 0;
+		//     left++;
+		//     right--;
+		// }
+		// return lCount==rCount;
+        
+        Set VOWELS = Set.of('a','e','i','o','u','A','E','I','O','U');
 		int lCount = 0, rCount = 0;
-		while(left<right){
-		    lCount += VOWELS.contains(s.charAt(left)) ? 1 : 0;
-		    rCount += VOWELS.contains(s.charAt(right)) ? 1 : 0;
-		    left++;
-		    right--;
+		for(int i=0;i<s.length()/2;i++){
+		  lCount += VOWELS.contains(s.charAt(i)) ? 1 : 0;
+		  rCount += VOWELS.contains(s.charAt(i+s.length()/2)) ? 1 : 0;
 		}
-		return lCount==rCount;        
+		return lCount==rCount;
     }
 }
