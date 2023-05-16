c1074f0b64dba7e7ef2afa4842fc921e595ea4ec
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-16 16:28:18 +0530

    Time: 2 ms (58.38%), Space: 46.8 MB (63.41%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,20 +1,16 @@
-class Solution {
-    
-    public static int Bits(int n){
-        int c=0;
-        while(n!=0){
-            c+=(n&1);
-            n=n>>1;
-        }
-        return c;
-    }
-    
+class Solution {    
     public int[] countBits(int n) {
-		int[] output = new int[n+1];
-		for(int i=0;i<=n;i++){
-		    System.out.println("Value of i->"+i);
-		    output[i] = Bits(i);
-		}  
+		int [] output = new int[n+1];
+		for(int i=0;i<=n;i++) {
+		    if(i==0)
+		        output[0] = 0;
+		    else if(i==1)
+		        output[1] = 1;
+		    else {
+		        int temp = (i%2==0) ? output[i/2] : output[i/2]+1;
+		        output[i] = temp;
+		    }
+		}
         return output;        
     }
 }
