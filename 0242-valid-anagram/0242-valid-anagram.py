39ecb5f47e1b0bf76d70a65046c70de5d24bd720
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-17 17:51:45 +0530

    Time: 60 ms (44.26%), Space: 16.7 MB (41.11%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)):
+            return False
+        else:
+            array = [0 for x in range(26)]
+            for char in s:
+                array[ord(char)-97]+=1
+            for char in t:
+                array[ord(char)-97]-=1
+            for ele in array:
+                if ele != 0:
+                    return False
+            return True
