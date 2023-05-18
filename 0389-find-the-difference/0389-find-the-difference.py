2a4155545a1145449c1f13f9a3356e30b05fd357
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-18 19:22:45 +0530

    Time: 46 ms (35.64%), Space: 16.4 MB (12.53%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,19 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        alpha = [0 for x in range(26)]
-        for char in s:
-            alpha[ord(char)-97]+=1
-        for char in t:
-            alpha[ord(char)-97]-=1
-        for ele in range(len(alpha)):
-            if alpha[ele] != 0:
-                return chr(ele+97)
+        # Using Bit Manipulation
+        output = 0
+        output ^= ord(t[-1])
+        for i in range(len(s)):
+            output ^= ord(s[i])
+            output ^= ord(t[i])
+        return chr(output)
+
+        # Using Arrays
+        # alpha = [0 for x in range(26)]
+        # for char in s:
+        #     alpha[ord(char)-97]+=1
+        # for char in t:
+        #     alpha[ord(char)-97]-=1
+        # for ele in range(len(alpha)):
+        #     if alpha[ele] != 0:
+        #         return chr(ele+97)
