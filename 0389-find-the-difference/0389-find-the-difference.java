c788f91353c6532c6738352b3ab7b4000eb374d0
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-18 19:22:04 +0530

    Time: 2 ms (69.45%), Space: 41.4 MB (6.81%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,16 +1,25 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-	    int[] alpha = new int[26];
-        char res = Character.MIN_VALUE;
-	    for(int i=0;i<s.length();i++)
-	        alpha[s.charAt(i)-'a']++;
-	    for(int i=0;i<t.length();i++)
-	        alpha[t.charAt(i)-'a']--;
-	    for(int i=0; i<26;i++) {
-            if(alpha[i]!=0) {
-                res = (char)(i+'a');
-            }
-        }
-        return res;
+	    int output = 0;
+	    output ^= (int)t.charAt(t.length()-1);
+	    for(int i=0;i<s.length();i++) {
+	        output ^= (int)s.charAt(i);
+	        output ^= (int)t.charAt(i);
+	    }
+	    return (char)output;
+        
+        // int[] alpha = new int[26];
+	    // char res = Character.MIN_VALUE;
+	    // for(int i=0;i<s.length();i++)
+	    //     alpha[s.charAt(i)-'a']++;
+	    // for(int i=0;i<t.length();i++)
+	    //     alpha[t.charAt(i)-'a']--;
+	    // for(int i=0; i<26;i++) {
+	    // if(alpha[i]!=0) {
+	    // res = (char)(i+'a');
+	    // }
+	    // }
+	    // return res;
+          
     }
 }
