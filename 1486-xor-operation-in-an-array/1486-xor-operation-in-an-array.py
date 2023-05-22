bf31af9acf07a548d1ff0faa99bd933274455e0a
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-22 12:07:53 +0530

    Time: 43 ms (27.57%), Space: 16.3 MB (22.81%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        res = 0
+        for i in range(n):
+            res^=(start+2*i)
+        return res
+    
+# Time Complexity = O(n)
+# Space Complexity = O(1)
