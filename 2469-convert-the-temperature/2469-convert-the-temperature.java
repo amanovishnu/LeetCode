544ce6aee6b8c9e82edcae6adc872f033572e9f5
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-22 11:58:14 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (39.64%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,3 +1,7 @@
-class Solution:
-    def convertTemperature(self, celsius: float) -> List[float]:
-        return [celsius+273.15, celsius*1.80+32.00]
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius+273.15, celsius*1.80+32.00};
+    }
+}
+// Time Complexity = O(1)
+// Space Complexity = O(1)
