5d50800a4e3bb63b4d07a5452beb0dc76ea4fc94
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-24 11:28:13 +0530

    Time: 130 ms (87.58%), Space: 19 MB (19.54%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res^=num
+        return res
+
+# Time Complexity: O(n)
+# Space Complexity = O(1)
+        
