c0dbbb612a20bda4c7a84acd71bbe2ab4ca65b02
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-05-24 14:19:01 +0530

    Time: 1 ms (99.72%), Space: 44.8 MB (5.22%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,16 +1,19 @@
-class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
-        res = []
-        for i in range(1,n+1):
-            if i%3==0 and i%5==0:
-                res.append("FizzBuzz")
-            elif i%3==0:
-                res.append("Fizz")
-            elif i%5==0:
-                res.append("Buzz")
-            else:
-                res.append(str(i))
-        return res
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%15==0)
+                res.add("FizzBuzz");
+            else if(i%3==0)
+                res.add("Fizz");
+            else if(i%5==0)
+                res.add("Buzz");
+            else
+                res.add(Integer.toString(i));
+        }
+        return res;
+    }
+}
 
-# Time Complexity: O(n)
-# Space Complexity: O(n)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
