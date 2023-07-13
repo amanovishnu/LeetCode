940a40c62a2ab76795f7899eb329af1b378affce
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-07-13 09:44:12 +0530

    Time: 112 ms (74.56%), Space: 17.7 MB (90.61%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for _ in range(n)]
+        indegree = [0] * n
+        ans = []
+
+        for pair in prerequisites:
+            course = pair[0]
+            prerequisite = pair[1]
+            adj[prerequisite].append(course)
+            indegree[course] += 1
+
+        queue = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        while queue:
+            current = queue.popleft()
+            ans.append(current)
+
+            for next_course in adj[current]:
+                indegree[next_course] -= 1
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        return len(ans) == n
