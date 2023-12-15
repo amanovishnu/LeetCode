0761d351eb1afc1a94e170eb2b0162a38b87e434
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-12-15 18:12:22 +0530

    Time: 57 ms (82.46%), Space: 16.4 MB (45.28%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -3,6 +3,6 @@ class Solution:
         if len(paths) == 1:
             return paths[0][1]
         else:
-            source = set([path[0] for path in paths])
-            destination = set([path[1] for path in paths])
-            return ''.join(destination.difference(source))
+            transposed_paths = zip(*paths)
+            source_list, destination_list = map(set, transposed_paths)
+            return destination_list.difference(source_list).pop()
