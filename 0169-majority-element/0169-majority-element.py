04660b983055eb3a82b4a99081b210647f54ebe9
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-12-19 19:06:20 +0530

    Time: 160 ms (18.30%), Space: 17.9 MB (53.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,26 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        ele_dict = {}
-        nums_length = len(nums)  # O(n)
-        for ele in nums: # O(n)
-            if ele in ele_dict.keys(): # O(1)
-                ele_dict[ele] += 1
-            else:
-                ele_dict[ele] = 1
-        return (max([key for key, value in ele_dict.items() if value >= nums_length/2]))
+        len_nums: int = len(nums)
+        if len_nums == 1:
+            return nums[0]
+        else:
+            nums_dict:dict = defaultdict(int)
+            for ele in nums:
+                nums_dict[ele] += 1
+            output:int = [key for key, value in nums_dict.items() if value > floor(len_nums/2)]
+            return output[0]
+        
+        
+        # Approach #1 
+        # nums_length = len(nums)
+        # if nums_length == 1:
+        #     return nums[0]
+        # else:
+        #     nums.sort() # TC: O(nlogn) Uses timsort inplace sorting AS: O(logn)
+        #     majority_element_idx: int = floor(nums_length/2) # TC: O(1) len info is stored in metadata
+        #     return nums[majority_element_idx] # TC: O(1)
+
+# Analysis Approach 1:    
+# 1. Time Complexity: O(nlogn)
+# 2. Auxilary Space: O(logn)
+# 3. Space Complexity: O(n+logn) 
