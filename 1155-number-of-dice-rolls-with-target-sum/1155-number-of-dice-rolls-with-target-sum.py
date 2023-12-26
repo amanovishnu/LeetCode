e016488fe5a7553e808a1b1d47cf58c97d3f8a01
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-12-26 22:03:10 +0530

    Time: 282 ms (63.38%), Space: 17.8 MB (48.66%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    mod = 10 ** 9 + 7
+
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        dp = [[-1] * (target + 1) for _ in range(n + 1)]
+        return self.recursion(dp, n, k, target)
+
+    def recursion(self, dp: list, n: int, k: int, target: int) -> int:
+        if target == 0 and n == 0:
+            return 1
+        if n == 0 or target <= 0:
+            return 0
+
+        if dp[n][target] != -1:
+            return dp[n][target] % self.mod
+
+        ways = 0
+        for i in range(1, k + 1):
+            ways = (ways + self.recursion(dp, n - 1, k, target - i)) % self.mod
+
+        dp[n][target] = ways % self.mod
+        return dp[n][target]
