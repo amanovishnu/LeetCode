c21c4c844365f6dd6d4f793401991666553921cd
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-12-28 19:10:44 +0530

    Time: 46 ms (21.89%), Space: 17.1 MB (8.34%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,16 +1,33 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        if len(nums) == len(set(nums)):
+        # Apporach #1
+        # if len(nums) == len(set(nums)):
+        #     return 0
+        # else:
+        #     pair_counter: int = 0
+        #     for i in range(len(nums)):
+        #         for j in range(i+1, len(nums)):
+        #             if nums[i] == nums[j]:
+        #                 pair_counter += 1
+        #     return pair_counter
+        
+        pairs: dict = defaultdict(int)
+        pair_counter: int = 0
+        for num in nums:
+            pairs[num] +=1
+        if len(nums) == (len(pairs)):
             return 0
         else:
-            pair_counter: int = 0
-            for i in range(len(nums)):
-                for j in range(i+1, len(nums)):
-                    if nums[i] == nums[j]:
-                        pair_counter += 1
+            for value in pairs.values():
+                pair_counter += value*(value-1)//2
             return pair_counter
         
-# Target: AMZN
+# Target: AMZN Approach #1
 # Time Complexity: O(n^2)
 # Auxilary Space: O(1)
 # Space Complexity: O(n)
+
+# Target: AMZN Approach #2
+# Time Complexity: O(n+m) in worst case its O(n)
+# Auxilary Space: O(m) in worst case its O(n)
+# Space Complexity: O(n+m) in worst case its O(n)
