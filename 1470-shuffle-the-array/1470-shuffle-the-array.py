70740934ec007e270621661314358f0c6d4a4c77
MANOVISHNU ADEPU <geekymano@gmail.com>
2023-12-28 18:45:39 +0530

    Time: 142 ms (5.41%), Space: 17.6 MB (7.15%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,12 +1,30 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        output: list = []
-        for i in range(n):
-            output.append(nums[i])
-            output.append(nums[n+i])
-        return output
+        # Approach #1
+        # output: list = []
+        # for i in range(n):
+        #     output.append(nums[i])
+        #     output.append(nums[n+i])
+        # return output
+        
+        # Approach #2
+        for i in range(n, len(nums)):
+            nums[i] |= nums[i-n] << 10
+        i = 0
+        for j in range(n, len(nums)):
+            nums[i] = nums[j] >> 10
+            nums[i+1] = nums[j] & 1023
+            i+=2
+        return nums
+        
     
-# Attempt = AMZN
+# Attempt = AMZN Approach #1
 # Time Complexity = O(n) where n is the input 
 # Auxilary Space = O(n)
+# Space Complexity = O(n)
+
+
+# Attempt = AMZN Approach #2
+# Time Complexity = O(n) where n is the input 
+# Auxilary Space = O(1)
 # Space Complexity = O(n)
