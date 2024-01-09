0097141031a2cff5955e6bf0a18ab3c08e9aa5f7
MANOVISHNU ADEPU <geekymano@gmail.com>
2024-01-09 21:44:04 +0530

    Time: 35 ms (83.93%), Space: 17.3 MB (28.86%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        return self.findleaf(root1) == self.findleaf(root2)
+        
+    def findleaf(self, root):
+        if not root: return []
+        if not (root.left or root.right): return [root.val]
+        return self.findleaf(root.left) + self.findleaf(root.right)
